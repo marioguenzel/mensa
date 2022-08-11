7cfb09f1546d443fa30b9c49541b2102f38ee73b
Mario Günzel <mario@MacBook-Air-von-Mario.local>
2022-08-11 10:41:09 +0200

    mensa script

---
+++ mensa_plan.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+# pip install beautifulsoup4
+# pip install requests
+import requests
+from bs4 import BeautifulSoup
+from optparse import OptionParser
+import datetime
+
+
+def find_meals_and_prices(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    rows = soup.find_all('tbody')  # table bodies
+    rows = [row for body in rows for row in body.find_all('tr')]
+    rows = [(entry.find('td', 'meals__column-title'), entry.find_all('td', 'meals__column-price')) for entry in rows]
+    rows = [(entry[0].get_text(strip=True), [e.get_text(strip=True) for e in entry[1]]) for entry in rows
+            if entry[0] is not None]
+
+    return rows
+
+
+def find_date(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    date = soup.find('option', selected='selected')
+    return date.get_text(strip=True)
+
+
+# def print_meals_and_prices(meals_and_prices):
+#     for row in meals_and_prices:
+#         print(row[1][0], row[1][1], row[1][2], row[0])
+
+def print_plan(plan_list):
+    for row in plan_list:
+        print(row[0], row[1])
+
+
+def get_request(date):
+    """date in format: 2021-08-26"""
+    url = f"https://mobil.itmc.tu-dortmund.de/canteen-menu/v3/canteens/341/{date}"
+    response = requests.request("GET", url)
+    return response.json()
+
+
+def beautify_mensa_plan(plan_dict):
+    """Beautify mensa plan from the dictionary."""
+    complete_list = []
+    for entry in plan_dict:
+        prices = entry['price']
+        complete_list.append(
+            [f"S: {prices['student']} B: {prices['staff']} G: {prices['guest']}", entry['title']['de']])
+
+    return complete_list
+
+
+if __name__ == '__main__':
+    date = str(datetime.date.today())
+    # options
+    parser = OptionParser()
+    parser.add_option("-d", "--date", dest="date", help="get mensa plan for DATE in the form yyyy-mm-dd",
+                      metavar="DATE")
+    parser.add_option("-p", "--plus", dest="plus", type='int', help="get mensa plan for today plus X days", metavar="X")
+
+    (options, args) = parser.parse_args()
+
+    if options.date is not None:
+        date = options.date
+
+    if options.plus is not None:
+        date = str(datetime.date.today() + datetime.timedelta(days=options.plus))
+
+    # # Request webpage
+    # session = requests.Session()
+    # res = session.get('https://www.stwdo.de/mensa-cafes-und-catering/tu-dortmund/hauptmensa/' + date)
+    #
+    # # Search info
+    # date = find_date(res.text)
+    # meals_and_prices = find_meals_and_prices(res.text)
+
+    # Get mensa plan as list of dictionaries
+    plan_dict = get_request(date)
+
+    # Beautify mensa plan
+    plan_list = beautify_mensa_plan(plan_dict)
+
+    # Print output
+    print(f'--Mensa plan for {date}--')
+    print_plan(plan_list)
